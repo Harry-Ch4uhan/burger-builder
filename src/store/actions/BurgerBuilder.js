a38aa82d96f9953c31a1ac431143034281443b29
Harry-Ch4uhan <85424351+Harry-Ch4uhan@users.noreply.github.com>
2021-07-30 13:08:30 +0530

    Update BurgerBuilder.js

--- src/store/actions/BurgerBuilder.js
+++ src/store/actions/BurgerBuilder.js
@@ -26,13 +26,7 @@ export const initIngredients = () => {
   return (dispatch) => {
     axios
       .get(
-        "https://burger-builder-f3576-default-rtdb.firebaseio.com/ingredients.json"
-        // {
-        //   headers: {
-        //     "Access-Control-Allow-Origin": "http://localhost:3000/",
-        //     "Referrer-Policy": "origin",
-        //   },
-        // }
+        "///"
       )
       .then((res) => {
         dispatch(setIngredients(res.data));
@@ -42,11 +36,3 @@ export const initIngredients = () => {
       });
   };
 };
-
-// "https://burger-builder-3162c-default-rtdb.firebaseio.com/Ingredients";
-// initIngredients({ bacon: 0, meat: 1, cheese: 0, salad: 0
-// {
-//           headers: {
-//             "Access-Control-Allow-Origin": "*",
-//           },
-//         }
